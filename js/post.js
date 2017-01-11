5956c8f50ceef22f84ff0c28e7f4b412449ddcd0
Programer <dahnenko02501@gmail.com>
2017-01-11 05:09:04 +0200

    1.0

---
+++ js/post.js
@@ -0,0 +1,8 @@
+function Post(title, imageUrl, description) {
+    this.title = title;
+    this.image = imageUrl;
+    this.description = description;
+
+}
+
+module.exports = Post;
---
+++ public/posts.js
@@ -0,0 +1,12 @@
+$("document").ready(function () {
+
+
+    $.get("/posts",function (result) {
+        console.log(result);
+
+
+    });
+
+
+
+});
